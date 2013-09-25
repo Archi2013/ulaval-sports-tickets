9cd489ebc9d5d7bb2936abca2fc426153961a54f
dpothier <dpothier@live.ca>
2013-09-25 13:25:09 -0400

    Ajout d'un filtre a liste de GameDto. C'est un WIP.

---
+++ src/main/java/dataFilters/GameDtoFilter.java
@@ -0,0 +1,17 @@
+package dataFilters;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.joda.time.DateTime;
+
+import ca.ulaval.glo4003.dtos.GameDto;
+
+public class GameDtoFilter {
+
+	public List<GameDto> FilterOnDate(DateTime UpwardBound, List<GameDto> listToFilter) {
+
+		return new ArrayList<GameDto>();
+	}
+
+}
