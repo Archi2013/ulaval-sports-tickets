15bd1997ca34c2c7941312337c587ee2d37c2dc2
nebble <nebble6@gmail.com>
2013-09-26 20:59:52 -0400

    Add new class to extract Sport from a XmlFile

---
+++ src/test/java/ca/ulaval/glo4003/xml/XmlSportDaoTest.java
@@ -0,0 +1,28 @@
+package ca.ulaval.glo4003.xml;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import ca.ulaval.glo4003.dtos.SportDto;
+
+public class XmlSportDaoTest {
+	private XmlSportDao sportDao = new XmlSportDao();
+	
+	@Test
+	public void testGet() throws Exception {
+		String sportName = "Hockey-Masculin";
+		SportDto sport = sportDao.get(sportName);
+		Assert.assertEquals(sportName, sport.getName());
+	}
+	
+	@Test
+	public void testGetAll() throws Exception {
+		List<SportDto> sports = sportDao.getAll();
+		Assert.assertEquals(3, sports.size());
+		Assert.assertEquals("Hockey-Masculin", sports.get(0).getName());
+		Assert.assertEquals("Baseball-Masculin", sports.get(1).getName());
+		Assert.assertEquals("Volleyball-Feminin", sports.get(2).getName());
+	}
+}
