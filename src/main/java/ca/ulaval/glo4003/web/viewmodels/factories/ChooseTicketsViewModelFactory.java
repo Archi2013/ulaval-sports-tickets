cf895fdde0fa2c1435f389463b22e48f2b417a6c
Mathieu Nivelles <mathieu.nivelles.1@ulaval.ca>
2013-11-04 22:32:42 -0500

    Ajout d'un fichier manquant.

---
+++ src/main/java/ca/ulaval/glo4003/web/viewmodels/factories/ChooseTicketsViewModelFactory.java
@@ -0,0 +1,29 @@
+package ca.ulaval.glo4003.web.viewmodels.factories;
+
+import org.springframework.stereotype.Component;
+
+import ca.ulaval.glo4003.domain.dtos.GameDto;
+import ca.ulaval.glo4003.domain.dtos.SectionDto;
+import ca.ulaval.glo4003.domain.utilities.Constants.TicketKind;
+import ca.ulaval.glo4003.web.viewmodels.ChooseTicketsViewModel;
+
+@Component
+public class ChooseTicketsViewModelFactory {
+
+	public ChooseTicketsViewModel createViewModel(GameDto gameDto, SectionDto sectionDto) {
+		ChooseTicketsViewModel chooseTicketsVM = new ChooseTicketsViewModel();
+		chooseTicketsVM.setNumberOfTicketsToBuy(1);
+		if (sectionDto.isGeneralAdmission()) {
+			chooseTicketsVM.setTicketKind(TicketKind.GENERAL_ADMISSION);
+		} else {
+			chooseTicketsVM.setTicketKind(TicketKind.WITH_SEAT);
+		}
+		chooseTicketsVM.setAdmissionType(sectionDto.getAdmissionType());
+		chooseTicketsVM.setSectionName(sectionDto.getSectionName());
+		chooseTicketsVM.setDate(gameDto.getGameDate());
+		chooseTicketsVM.setOpponents(gameDto.getOpponents());
+		chooseTicketsVM.setSport(gameDto.getSportName());
+		
+		return chooseTicketsVM;
+	}
+}
