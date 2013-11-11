d4f02e9ec5c2caebc477bafe74dacc38d8cd71d4
Mathieu Nivelles <mathieu.nivelles.1@ulaval.ca>
2013-11-11 18:44:38 -0500

    Correction d'un bogue. (@Component et implements ISectionFactory).

--- src/main/java/ca/ulaval/glo4003/domain/factories/SectionFactory.java
+++ src/main/java/ca/ulaval/glo4003/domain/factories/SectionFactory.java
@@ -1,10 +1,14 @@
 package ca.ulaval.glo4003.domain.factories;
 
+import org.springframework.stereotype.Component;
+
 import ca.ulaval.glo4003.domain.dtos.SectionDto;
 import ca.ulaval.glo4003.domain.pojos.Section;
 
-public class SectionFactory {
+@Component
+public class SectionFactory implements ISectionFactory {
 
+	@Override
 	public Section createSection(SectionDto sectionDto) {
 		return new Section(sectionDto);
 	}
