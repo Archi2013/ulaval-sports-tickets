35c5b1bfd5be0d6cf9bb6753642e50afd79c8e69
Mathieu Nivelles <mathieu.nivelles.1@ulaval.ca>
2013-11-12 02:56:09 -0500

    nombre magique.

--- src/main/java/ca/ulaval/glo4003/persistence/daos/TicketDoesntExistException.java
+++ src/main/java/ca/ulaval/glo4003/persistence/daos/TicketDoesntExistException.java
@@ -2,6 +2,6 @@ package ca.ulaval.glo4003.persistence.daos;
 
 public class TicketDoesntExistException extends Exception {
 
-	private static final long serialVersionUID = 1297881075595241152L;
+	private static final long serialVersionUID = 1L;
 
 }
