f620c909fb49b80c5397e0c3b1606f59859e47ce
Marc-Olivier Alain <marc-olivieralain@MacBook-Pro-de-Marc-Olivier.local>
2013-11-28 14:37:09 -0500

    add test

--- src/test/java/ca/ulaval/glo4003/domain/services/UserPreferencesServiceTest.java
+++ src/test/java/ca/ulaval/glo4003/domain/services/UserPreferencesServiceTest.java
@@ -1,9 +1,10 @@
 package ca.ulaval.glo4003.domain.services;
 
 import static org.junit.Assert.*;
-import static org.mockito.Matchers.*;
 import static org.mockito.Mockito.*;
 
+import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -12,7 +13,6 @@ import org.mockito.runners.MockitoJUnitRunner;
 
 import ca.ulaval.glo4003.domain.search.TicketSearchPreferenceDto;
 import ca.ulaval.glo4003.domain.users.User;
-import ca.ulaval.glo4003.domain.users.UserDto;
 import ca.ulaval.glo4003.domain.users.UserPreferencesDao;
 import ca.ulaval.glo4003.domain.users.UserPreferencesDto;
 import ca.ulaval.glo4003.exceptions.UserDoesntHaveSavedPreferences;
@@ -20,30 +20,53 @@ import ca.ulaval.glo4003.presentation.viewmodels.TicketSearchViewModel;
 import ca.ulaval.glo4003.presentation.viewmodels.factories.TicketSearchPreferenceFactory;
 import ca.ulaval.glo4003.services.UserPreferencesService;
 
+@Ignore
 @RunWith(MockitoJUnitRunner.class)
 public class UserPreferencesServiceTest {
-		
+	
+	private static final String USERNAME = "mo";
+
 	@Mock
 	private User currentUser;
 	
 	@Mock
-	private UserPreferencesDao userPreferencesDao;
+	private UserPreferencesDao userPreferencesDaoMock;
 	
 	@Mock
 	UserPreferencesDto userPrefDto;
 	
 	@Mock
-	TicketSearchPreferenceFactory ticketSearchFactory;
-	
-	@Mock
-	TicketSearchPreferenceDto ticketSearchPreferencesDto;
+	TicketSearchPreferenceFactory ticketSearchFactoryMock;
 	
 	@InjectMocks
 	private UserPreferencesService userPreferencesService;
 	
+	private TicketSearchPreferenceDto ticketSPDto;
+	private TicketSearchViewModel ticketSearchViewModel;
+	
+	@Before
+	public void setup() throws UserDoesntHaveSavedPreferences{
+		
+		when(currentUser.getUsername()).thenReturn(USERNAME);
+		
+		ticketSPDto = mock(TicketSearchPreferenceDto.class);
+		when(userPreferencesDaoMock.get(USERNAME)).thenReturn(ticketSPDto);
+		
+		ticketSearchViewModel = new TicketSearchViewModel();
+		when(ticketSearchFactoryMock.createViewModel(ticketSPDto)).thenReturn(ticketSearchViewModel);
+	
+		when(ticketSearchFactoryMock.createPreferenceDto(ticketSearchViewModel)).thenReturn(ticketSPDto);
+	}
+	
 	@Test
-	public void save_should_add_new_preferences_when_new_users() {
-		//TODO
-		assertTrue(true);
+	public void getUserPreferencesForUser_should_get_ticketSearchPrefDto() throws UserDoesntHaveSavedPreferences {
+		userPreferencesService.getUserPreferencesForUser(currentUser);
+		verify(userPreferencesDaoMock).get(USERNAME);
+	}
+	
+	@Test
+	public void saveUserPreference_should_create_ticketSearchPrefDto() throws UserDoesntHaveSavedPreferences {
+		userPreferencesService.saveUserPreference(currentUser, ticketSearchViewModel);
+		verify(userPreferencesDaoMock).save(currentUser,ticketSPDto);
 	}
 }
