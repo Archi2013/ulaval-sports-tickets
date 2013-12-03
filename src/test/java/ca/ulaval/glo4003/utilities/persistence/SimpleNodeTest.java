b4dfd99ce82acf19fe77c1dab07ba57c01b89a83
Carl <nebble6@gmail.com>
2013-12-02 21:03:31 -0500

    Move tests to good packages

--- src/test/java/ca/ulaval/glo4003/utilities/persistence/SimpleNodeTest.java
+++ src/test/java/ca/ulaval/glo4003/utilities/persistence/SimpleNodeTest.java
@@ -1,4 +1,4 @@
-package ca.ulaval.glo4003.persistence.xml;
+package ca.ulaval.glo4003.utilities.persistence;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
