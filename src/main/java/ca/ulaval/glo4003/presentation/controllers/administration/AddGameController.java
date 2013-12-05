30e77a7b2c8ad3e879cf173f1a5fa9f74c83fe5b
Mathieu Charest-Durand <mathieu.charest.durand@gmail.com>
2013-12-04 19:05:16 -0500

    Removed the connected verificator. No use = useless

--- src/main/java/ca/ulaval/glo4003/presentation/controllers/administration/AddGameController.java
+++ src/main/java/ca/ulaval/glo4003/presentation/controllers/administration/AddGameController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
-import ca.ulaval.glo4003.domain.users.User;
 import ca.ulaval.glo4003.exceptions.GameAlreadyExistException;
 import ca.ulaval.glo4003.exceptions.GameDoesntExistException;
 import ca.ulaval.glo4003.exceptions.NoSportForUrlException;
