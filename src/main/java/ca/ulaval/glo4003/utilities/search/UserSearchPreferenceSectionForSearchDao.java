d0cdf80e36cf75e0fbcc21d1c0e8dc281390da91
Carl <nebble6@gmail.com>
2013-12-07 22:09:46 -0500

    Change the name of the class and update method for newer code, should also improuve performance

---
+++ src/main/java/ca/ulaval/glo4003/utilities/search/UserSearchPreferenceSectionForSearchDao.java
@@ -0,0 +1,69 @@
+package ca.ulaval.glo4003.utilities.search;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.joda.time.DateTime;
+import org.springframework.stereotype.Repository;
+
+import ca.ulaval.glo4003.domain.game.GameDao;
+import ca.ulaval.glo4003.domain.game.GameDto;
+import ca.ulaval.glo4003.domain.sections.SectionDao;
+import ca.ulaval.glo4003.domain.sections.SectionDto;
+import ca.ulaval.glo4003.domain.sports.SportUrlMapper;
+import ca.ulaval.glo4003.domain.tickets.TicketTypeUrlMapper;
+import ca.ulaval.glo4003.utilities.time.UrlDateTime;
+import ca.ulaval.glo4003.utilities.time.UrlDateTimeFactory;
+
+@Repository
+class UserSearchPreferenceSectionForSearchDao implements SectionForSearchDao {
+
+	@Inject
+	GameDao gameDao;
+
+	@Inject
+	SectionDao sectionDao;
+
+	@Inject
+	TicketTypeUrlMapper ticketTypeUrlMapper;
+
+	@Inject
+	SportUrlMapper sportUrlMapper;
+
+	@Inject
+	UrlDateTimeFactory urlDateTimeFactory;
+
+	@Override
+	public List<SectionForSearchDto> getSections(UserSearchPreferenceDto ticketSearchPreferenceDto) {
+		List<SectionForSearchDto> sectionsForSearches = new ArrayList<>();
+		
+		List<String> ticketKinds = ticketSearchPreferenceDto.getSelectedTicketKinds();
+		List<GameDto> games = gameDao.getFromUserSearchPreference(ticketSearchPreferenceDto);
+		for (GameDto game : games) {
+			sectionsForSearches.addAll(findInSectionsForTicketKinds(ticketKinds, game));
+		}
+
+		return sectionsForSearches;
+	}
+
+	private List<SectionForSearchDto> findInSectionsForTicketKinds(List<String> ticketKinds, GameDto game) {
+		List<SectionForSearchDto> sectionsForSearches = new ArrayList<>();
+		
+		List<SectionDto> sections = sectionDao.getAllSectionsForTicketKind(game.getSportName(), game.getGameDate(), ticketKinds);
+		for (SectionDto section : sections) {
+			String url = createUrl(game.getSportName(), game.getGameDate(), section.getSectionName());
+			SectionForSearchDto sectionForSearch = new SectionForSearchDto(section, game, game.getSportName(), url);
+			sectionsForSearches.add(sectionForSearch);
+		}
+		
+		return sectionsForSearches;
+	}
+
+	private String createUrl(String sportName, DateTime gameDate, String sectionName) {
+		UrlDateTime urlDateTime = urlDateTimeFactory.create(gameDate);
+		return String.format("/sport/%s/match/%s/billets/%s", sportUrlMapper.getUrl(sportName), urlDateTime.toString(),
+				ticketTypeUrlMapper.getUrl(sectionName));
+	}
+}
